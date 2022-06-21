628234ba13a9b26008e7a5bd5d0725a97face85c
Dillon Brock <mdillon1297@gmail.com>
2022-06-21 16:52:47 -0700

    built out handlePageLoad()

--- detail/detail.js
+++ detail/detail.js
@@ -11,14 +11,14 @@ let dog = {};
 // write handler functions
 async function handlePageLoad() {
     // *** Get the id from search params and assign to "id" variable
-
-
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
     if (!id) window.location = '/';
 
     // *** Use the id to get this dog (async, so you need to "await"!)
     // and assign to "dog" variable
 
-
+    dog = await getDog(id);
     if (!dog) window.location = '/';
 
     display();
